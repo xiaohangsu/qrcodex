8d1feb27b285be830f3e42b951df5be9289935b4
kiadragon <sxhdragon@gamil.com>
2015-02-01 19:32:30 +0800

    config.js & use JQ NOT JQ-MOBILE

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+  app_name: 'QRCODEX',
+  port: 3000,
+  avos_app_id: '4jqce5lbpwlc134doriax0mrj2nzalh0nnr07a7uq5lpxro0',
+  avos_app_key: '1wvx7b6wgzk3bwk1agwo2a3afavp5gn7n41izj91nfjyij78',
+  avos_master_key: 's32xzjxyvufzkj2vr4mp2tfue2si5u9k33qufxq1zv3yeup5'
+};
+
+module.exports = config;
