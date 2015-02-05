492c5502519f32a4654a01d3ca0ad76e67f65fc9
kiadragon <sxhdragon@gamil.com>
2015-02-05 14:07:52 +0800

    Specify only one admin account & url hash

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
+var crypto = require('crypto');
 var AV = require('avoscloud-sdk').AV;
+
+var Hash_Code = '';
+
 /* GET admin-login page. */
 router.get('/login', function(req, res, next) {
   res.render('admin_login', {
@@ -9,11 +14,12 @@ router.get('/login', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
-  console.log(req.body);
   AV.User.logIn(req.body.admin_name, req.body.admin_password, {
     success: function(user) {
-      console.log(user);
-      res.redirect('/admin/add');
+      var hash = crypto.createHash("md5");
+      hash.update(new Date().getTime().toString());
+      Hash_Code = hash.digest('hex');
+      res.redirect('/admin/add' + '/' + Hash_Code);
     },
     error: function(user, error) {
       res.redirect('/admin/login');
@@ -21,9 +27,14 @@ router.post('/login', function(req, res, next) {
   });
 });
 
-router.get('/add', function(req, res, next) {
-  res.render('admin_add', {
-    title: 'Admin - Add'
-  });
+
+router.get('/add/:hash_code', function(req, res, next) {
+  if (req.params.hash_code === Hash_Code) {
+    res.render('admin_add', {
+      title: 'Admin - Add'
+    });
+  } else {
+    res.redirect('/admin/login');
+  }
 });
 module.exports = router;
